9ce8fc0e4d1bcaaa06220b068efb3ab58018ba81
portfolio <jwjs3219@gmail.com>
2022-03-05 21:13:04 +0900

    AOP 작업 시작

--- photogram/src/main/java/com/cos/photogramstart/handler/aop/ValidationAdvice.java
+++ photogram/src/main/java/com/cos/photogramstart/handler/aop/ValidationAdvice.java
@@ -1,7 +1,13 @@
 package com.cos.photogramstart.handler.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
 // Validaion == '유효성' Advice == '공통기능'
 
+@Component // RestController,Service 등 모든 것들이 Component를 상속해서 만들었음.
+@Aspect
 public class ValidationAdvice {
-
+	
+	
 }
--- photogram/src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
+++ photogram/src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 // NotNull = Null값 체크
-// NotBlank= 빈값 or Null or 공백 체크
+// NotBlank= 빈값 or Null or 공백(스페이스) 체크
 // NotEmpty= 빈값 or null 체크
 @Data
 public class CommentDto {
